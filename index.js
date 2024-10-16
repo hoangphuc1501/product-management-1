f2a3fa541911056a4f05d8cfcf96deb61458418f
hoangphuc1501 <katakurilancer.com>
2024-10-16 10:48:24 +0700

    Cấu hình folder route

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const routeClient = require("./routes/client/index.route");
 app.set('views', './views'); //Tìm đến thư mục tên là view
 app.set('view engine', 'pug')// template engine sử dụng là pug
 
-app.get("/", (req, res) => {
-    res.render("client/pages/home/index.pug");
-});
-
-app.get("/products", (req, res) => {
-    res.render("client/pages/products/index.pug");
-});
+// routeClient(app) // khi ko có đặt tên hàm thì biến routeClinet chính là tên hàm
+routeClient.index(app) // khi có đặt tên hàm
 
 
 app.listen(port, ()=>{
