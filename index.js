7e3c7d7b8afedfa6894ac48e74690263578a403f
hoangphuc1501 <katakurilancer.com>
2024-10-16 17:16:30 +0700

    sửa layout

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT;
 const routeClient = require("./routes/client/index.route");
 app.set('views', './views'); //Tìm đến thư mục tên là view
 app.set('view engine', 'pug')// template engine sử dụng là pug
+app.use(express.static('public'))// thiết lập thư mục chứa file tĩnh
 
 // routeClient(app) // khi ko có đặt tên hàm thì biến routeClinet chính là tên hàm
 routeClient.index(app) // khi có đặt tên hàm
