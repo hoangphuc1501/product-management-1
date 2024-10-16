df5fd48299e9bea564c09b0207193f5341616508
hoangphuc1501 <katakurilancer.com>
2024-10-16 18:13:34 +0700

    làm trang danh sách sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,17 @@
-module.exports.index =  (req, res) => {
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted: false
+    });
+
+    for (const item of products) {
+        item.priceNew = item.price*(100-item.discountPercentage)/100;
+        item.priceNew = (item.priceNew).toFixed(0)
+    }    
+    console.log(products)
     res.render("client/pages/products/index.pug", {
-        pageTitle: "Trang danh sách sán phẩm" 
+        pageTitle: "Trang danh sách sán phẩm",
+        products: products
     });
 }
