9b490d8a0b570e0d0252d2eecf81127e840ca30c
hoangphuc1501 <katakurilancer.com>
2024-10-16 08:56:10 +0700

    Cài đặt pug + views

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set('views', './views'); //Tìm đến thư mục tên là view
+app.set('view engine', 'pug')// template engine sử dụng là pug
 
 app.get("/", (req, res) => {
-    res.send("trang chủ");
+    res.render("client/pages/home/index.pug");
 });
 
 app.get("/products", (req, res) => {
-    res.send("Trang danh sách sản phẩm");
+    res.render("client/pages/products/index.pug");
 });
 
 
