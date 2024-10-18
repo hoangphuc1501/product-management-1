cf85b020f1d1ecab4a9c6bc53f20fe97a4441ea1
hoangphuc1501 <katakurilancer.com>
2024-10-18 15:07:13 +0700

    tính năng tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -3,7 +3,6 @@ const Product = require("../../models/product.model");
 module.exports.index = async (req, res) => {
     const find = {
         deleted: false
-        
     }
     // lọc theo trạng thái
     if(req.query.status) {
@@ -11,6 +10,12 @@ module.exports.index = async (req, res) => {
     }
     // hết lọc theo trạng thái
     
+    // Tìm kiếm
+    if(req.query.keyword){
+        const regex = new RegExp(req.query.keyword, "i")
+        find.title = regex;
+    }
+    // hết tìm kiếm
 
     const products = await Product.find(find)
 
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -23,6 +23,29 @@ if(boxFilter){
         boxFilter.value = statusCurrent;
     }
 }
-
-
 // hết bộ lọc
+
+// Tìm kiếm
+    const formSearch = document.querySelector("[form-search]");
+    if(formSearch){
+
+        let url = new URL(location.href) // nhân bản đường link
+        formSearch.addEventListener("submit", (event) => {
+            event.preventDefault(); // ngăn chặn hành vi mặc định: submit fomr chuyển hướng đi
+            const value = formSearch.keyword.value;
+            if(value){
+                url.searchParams.set("keyword", value);
+            }else{
+                url.searchParams.delete("keyword");
+            }
+            location.href = url.href; // chuyển hướng đến đường link mới
+        })
+        // hiển thị từ khóa mặc định
+        const valueCurrent = url.searchParams.get("keyword");
+        if(valueCurrent) {
+            formSearch.keyword.value = valueCurrent;
+        }
+
+        
+    }
+// hết tìm kiếm
