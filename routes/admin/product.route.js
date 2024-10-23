cd960903c9a0ef416af49b31e042179fd067ca0b
hoangphuc1501 <katakurilancer.com>
2024-10-23 17:44:56 +0700

    up hình lên cloud

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const route = express.Router();
 const multer = require('multer');
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './public/uploads/')
-    },
-    filename: function (req, file, cb) {
-        const fileName = `${Date.now()}-${file.originalname}`;
-        cb(null, fileName)
-    }
-});
-const upload = multer({ storage: storage });
+
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, './public/uploads/')
+//     },
+//     filename: function (req, file, cb) {
+//         const fileName = `${Date.now()}-${file.originalname}`;
+//         cb(null, fileName)
+//     }
+// });
+// const upload = multer({ storage: storage });
+const upload = multer();
+const uploadCloud = require("../../middlewares/admin/uploadcloud.middleware");
 const validate = require("../../validates/admin/product.validate");
 const controller = require("../../controllers/admin/product.controller");
 
@@ -23,14 +27,20 @@ route.patch("/change-multi", controller.changeMulti);
 route.patch("/delete", controller.delete);
 route.patch("/change-position", controller.changePosition);
 route.get("/create", controller.create);
-
+route.post(
+    "/create",
+    upload.single('thumbnail'),
+    uploadCloud.uploadSingle,
+    validate.createPost,
+    controller.createPost);
 
 route.get("/edit/:id", controller.edit);
 route.patch(
-    "/edit/:id", 
+    "/edit/:id",
     upload.single('thumbnail'),
-    validate.createPost, 
-    controller.editPatch);    
+    uploadCloud.uploadSingle,
+    validate.createPost,
+    controller.editPatch);
 
 route.get("/detail/:id", controller.detail);
 
