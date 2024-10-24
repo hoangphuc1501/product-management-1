b570b002b47778ce41214e4557710132ae3fb5a5
hoangphuc1501 <katakurilancer.com>
2024-10-24 14:26:26 +0700

    Xây dụng thêm mới nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -1,4 +1,6 @@
 const Role = require("../../models/role.model");
+const systemComfig = require("../../config/system");
+
 module.exports.index = async (req, res)=>{
     const records = await Role.find({
         deleted: false
@@ -8,4 +10,17 @@ module.exports.index = async (req, res)=>{
         pageTitle: "Nhóm quyền",
         records: records
     });
+}
+
+module.exports.create = async (req, res)=>{
+
+    res.render("admin/pages/roles/create", {
+        pageTitle: "Tạo nhóm quyền",
+    });
+}
+
+module.exports.createPost = async (req, res)=>{
+    const role = new Role(req.body);
+    await role.save();
+    res.redirect(`/${systemComfig.prefixAdmin}/roles`);
 }
