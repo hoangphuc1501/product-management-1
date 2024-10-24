c817fcc9e6894c1107f21a6655ee77a28c762e08
hoangphuc1501 <katakurilancer.com>
2024-10-24 14:01:16 +0700

    Tạo danh sách roles

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,7 +1,8 @@
 const dashboardRoute = require("./dashboard.route");
-const productRoute = require("./product.route")
-const produtCategoryRoute = require("./product-category.route")
-const systemConfig = require("../../config/system")
+const productRoute = require("./product.route");
+const produtCategoryRoute = require("./product-category.route");
+const role = require("./role.route");
+const systemConfig = require("../../config/system");
 
 module.exports = (app) => {
     const PATH_ADMIN = `/${systemConfig.prefixAdmin}`;
@@ -9,4 +10,5 @@ module.exports = (app) => {
     app.use(`${PATH_ADMIN}/dashboard`, dashboardRoute);
     app.use(`${PATH_ADMIN}/products`, productRoute);
     app.use(`${PATH_ADMIN}/products-category`, produtCategoryRoute);
+    app.use(`${PATH_ADMIN}/roles`, role);
 }
