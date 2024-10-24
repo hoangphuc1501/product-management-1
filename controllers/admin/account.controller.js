6dad5a2526060484a67b27a55bb5811cf09ce04f
hoangphuc1501 <katakurilancer.com>
2024-10-24 17:05:33 +0700

    Danh sách tài khoản

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -4,13 +4,25 @@ const Account = require("../../models/account.model");
 const generateHelper = require("../../helpers/generate.helper");
 const systemConfig = require("../../config/system");
 const md5 = require('md5');
-module.exports.index = (req, res)=>{
+module.exports.index = async (req, res) => {
+    const records = await Account.find({
+        deleted: false
+    });
+    for (const item of records) {
+        const role = await Role.findOne({
+            _id: item.role_id,
+            deleted: false
+        });
+        item.role_title = role.title;
+    }
+
     res.render("admin/pages/accounts/index", {
-        pageTitle: "Tài khoản quản trị"
+        pageTitle: "Tài khoản quản trị",
+        records: records
     });
 }
 
-module.exports.create = async (req, res)=>{
+module.exports.create = async (req, res) => {
     const roles = await Role.find({
         deleted: false
     })
@@ -25,8 +37,8 @@ module.exports.create = async (req, res)=>{
 module.exports.createPost = async (req, res) => {
     req.body.password = md5(req.body.password);
     req.body.token = generateHelper.generateRandomString(30);
-    
+
     const account = new Account(req.body);
     await account.save();
-    res.redirect(`/${systemConfig.prefixAdmin}/accounts`); 
+    res.redirect(`/${systemConfig.prefixAdmin}/accounts`);
 }
